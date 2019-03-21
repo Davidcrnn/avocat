3ad06b08bc02511bf0b88cfcbc0f5016efc6f937
David Crenin <david.crenin@gmail.com>
2019-03-21 09:53:35 +0100

    actualites controller

--- app/models/actualite.rb
+++ app/models/actualite.rb
@@ -2,5 +2,7 @@ class Actualite < ApplicationRecord
   validates :title, presence: true
   validates :contenu, presence: true
   validates :auteur, presence: true
-  validates :date, presence: true
+  validates :day, presence: true
+  validates :month, presence: true
+  validates :year, presence: true
 end
